a2778d2e6490895d63def16cf730fdebe829a00c
HIJIKIsw <hijiki_seaweed@yahoo.co.jp>
2017-02-07 20:23:35 +0900

    Version 1.1.0 ・ActionBarAPI を使用しないように変更 ・RotationMessage のデフォルト値に装飾コードを追加

--- src/hijiki_seaweed/Rotation_Hotbar/Main.java
+++ src/hijiki_seaweed/Rotation_Hotbar/Main.java
@@ -10,6 +10,7 @@ import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -19,46 +20,58 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.connorlinfoot.actionbarapi.ActionBarAPI;
+import net.minecraft.server.v1_11_R1.ChatComponentText;
+import net.minecraft.server.v1_11_R1.PacketPlayOutChat;
 
-public class Main extends JavaPlugin implements Listener {
+/*
+ * Main クラス
+ */
+public class Main extends JavaPlugin implements Listener
+{
+	/*
+	 * メンバー変数の宣言
+	 */
+	private static Map<Player, Long> AntiClickSpamDelay = new HashMap<Player, Long>(); // 連打防止用
 
-	// 連打防止
-	private static Map<Player, Long> AntiClickSpamDelay = new HashMap<Player, Long>();
-
+	/*
+	 * プラグインが Load されたとき
+	 */
 	public void onEnable()
 	{
 		this.saveDefaultConfig();
 		ConsoleCommandSender c = Bukkit.getConsoleSender();
 		FileConfiguration Config = getConfig();
 
-		if( !Config.isSet("RotationMessage") )
+		if( !Config.isSet( "RotationMessage" ) )
 		{
-			getConfig().set("RotationMessage", "ホットバーをローテーションしました。");
-			c.sendMessage("config.yml に RotationMessage が存在しなかったため追加しました。");
+			getConfig().set( "RotationMessage", "'&7&lホットバーをローテーションしました。'" );
+			c.sendMessage( "config.yml に RotationMessage が存在しなかったため追加しました。" );
 		}
 
-		if( !Config.isSet("AntiClickSpamDelay") )
+		if( !Config.isSet( "AntiClickSpamDelay" ) )
 		{
-			getConfig().set("AntiClickSpamDelay", 50);
-			c.sendMessage("config.yml に AntiClickSpamDelay が存在しなかったため追加しました。");
+			getConfig().set( "AntiClickSpamDelay", 50 );
+			c.sendMessage( "config.yml に AntiClickSpamDelay が存在しなかったため追加しました。" );
 		}
 
 		saveConfig();
 
-		Bukkit.getPluginManager().registerEvents(this, this);
+		Bukkit.getPluginManager().registerEvents( this, this );
 	}
 
+	/*
+	 * プラグインが Unload されたとき
+	 */
 	public void onDisable()
 	{
 		// null
 	}
 
-	@EventHandler
-	(
-		priority = EventPriority.HIGHEST
-	)
-	public void onSwapHandItem(final PlayerSwapHandItemsEvent e)
+	/*
+	 * プレイヤーがメインハンドとオフハンドのアイテムを入れ替えた時 (F キー)
+	 */
+	@EventHandler( priority = EventPriority.HIGHEST )
+	public void onSwapHandItem( final PlayerSwapHandItemsEvent e )
 	{
 		Player Player = e.getPlayer();
 
@@ -68,14 +81,13 @@ public class Main extends JavaPlugin implements Listener {
 		// スニークしている場合
 		if( isSneaking )
 		{
-
 			// イベントをキャンセル
-			e.setCancelled(true);
+			e.setCancelled( true );
 
 			// 連打を防止
 			Long Now = System.currentTimeMillis();
-			int MinDelay = getConfig().getInt("AntiClickSpamDelay");
-			Long CoolDownUntil = AntiClickSpamDelay.get(Player);
+			int MinDelay = getConfig().getInt( "AntiClickSpamDelay" );
+			Long CoolDownUntil = AntiClickSpamDelay.get( Player );
 			if( MinDelay > 0 )
 			{
 				if( CoolDownUntil != null && CoolDownUntil > Now )
@@ -84,7 +96,7 @@ public class Main extends JavaPlugin implements Listener {
 				}
 				else
 				{
-					AntiClickSpamDelay.put(Player, Now+MinDelay);
+					AntiClickSpamDelay.put( Player, Now + MinDelay );
 				}
 			}
 
@@ -95,10 +107,10 @@ public class Main extends JavaPlugin implements Listener {
 			ItemStack Items[] = new ItemStack[36];
 			for( int i = 0; i < Items.length; i++ )
 			{
-				Items[i] = Inventory.getItem(i);
+				Items[i] = Inventory.getItem( i );
 				if( Items[i] == null )
 				{
-					Items[i] = new ItemStack(Material.AIR);
+					Items[i] = new ItemStack( Material.AIR );
 				}
 			}
 
@@ -108,40 +120,43 @@ public class Main extends JavaPlugin implements Listener {
 				// ホットバー行の場合
 				if( i > 8 )
 				{
-					Inventory.setItem(i, Items[i-9]);
+					Inventory.setItem( i, Items[i - 9] );
 				}
 				else
 				{
-					Inventory.setItem(i, Items[27+i]);
+					Inventory.setItem( i, Items[27 + i] );
 				}
 
 			}
 
 			// サウンド再生
 			World World = Player.getWorld();
-			World.playSound(Player.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 1.0f, 1.0f);
+			World.playSound( Player.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 1.0f, 1.0f );
 
 			// メッセージ表示
-			String RotationMessage = getConfig().getString("RotationMessage");
+			String RotationMessage = format( getConfig().getString( "RotationMessage" ) );
 			if( RotationMessage.length() > 0 )
 			{
-				// ActionBarAPI が Load されている場合
-				if( getServer().getPluginManager().getPlugin("ActionBarAPI") != null )
-				{
-					ActionBarAPI.sendActionBar(Player, format(RotationMessage));
-				}
-				// Load されていない場合
-				else
-				{
-					Player.sendMessage(format(RotationMessage));
-				}
+				sendActionBar( Player, RotationMessage );
 			}
 		}
 		return;
 	}
 
-	private static String format(String format)
+	/*
+	 * 文字列を Minecraft 内で使用できる形に整形する
+	 */
+	private static String format( String format )
 	{
-		return ChatColor.translateAlternateColorCodes('&', format);
+		return ChatColor.translateAlternateColorCodes( '&', format );
+	}
+
+	/*
+	 * 指定した player の ActionBar に Message を送信する
+	 */
+	private static void sendActionBar( Player player, String message )
+	{
+		PacketPlayOutChat packet = new PacketPlayOutChat( new ChatComponentText( message ), (byte)2 );
+		((CraftPlayer)player).getHandle().playerConnection.sendPacket( packet );
 	}
 }
